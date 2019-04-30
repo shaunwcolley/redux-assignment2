f84e28871ad3e522dee2bed5c1e15b82528c1979
Shaun W Colley <swc7838@gmail.com>
2019-04-30 11:33:04 -0500

    added in devtools argument to store for chrome

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reducer from './store/reducer'
 import { Provider } from 'react-redux'
 import Counter from './components/Counter'
 
-const store = createStore(reducer)
+const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <Provider store={store}>
